658696ec0b6bcc78bdba37ec883610370e099b77
Cezary Sanecki <devcezz@gmail.com>
2020-11-08 00:49:51 +0100

    Initial commit

---
+++ src/test/java/pl/csanecki/animalshelter/AnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.csanecki.animalshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

63950dcb0eda54d57d0ef58df27b1e7d05c6fa51
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-11-20 21:09:18 +0100

    Annotated controller - path animals

--- src/main/java/pl/csanecki/animalshelter/controller/AnimalRestController.java
+++ src/main/java/pl/csanecki/animalshelter/controller/AnimalRestController.java
@@ -1,4 +1,9 @@
 package pl.csanecki.animalshelter.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/animals")
 public class AnimalRestController {
 }

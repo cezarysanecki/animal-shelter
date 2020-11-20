f84af0baebf789a1cbc36405a0ecf9b96e187172
Cezary Sanecki <devcezz@gmail.com>
2020-11-20 23:56:05 +0100

    Added animal repository interface

---
+++ src/main/java/pl/csanecki/animalshelter/service/AnimalRepository.java
@@ -0,0 +1,4 @@
+package pl.csanecki.animalshelter.service;
+
+public interface AnimalRepository {
+}
--- src/main/java/pl/csanecki/animalshelter/service/AnimalServiceImpl.java
+++ src/main/java/pl/csanecki/animalshelter/service/AnimalServiceImpl.java
@@ -1,6 +1,14 @@
 package pl.csanecki.animalshelter.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import pl.csanecki.animalshelter.controller.AnimalSerivce;
 
 public class AnimalServiceImpl implements AnimalSerivce {
+
+    private AnimalRepository animalRepository;
+
+    @Autowired
+    public AnimalServiceImpl(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
+    }
 }

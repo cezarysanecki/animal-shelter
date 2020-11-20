cadb2702fe93d84ab5d5a28cc463c74be9780652
Cezary Sanecki <devcezz@gmail.com>
2020-11-21 00:13:54 +0100

    Injected jdbc template into animal repository

--- src/main/java/pl/csanecki/animalshelter/repository/AnimalRepositoryImpl.java
+++ src/main/java/pl/csanecki/animalshelter/repository/AnimalRepositoryImpl.java
@@ -1,6 +1,15 @@
 package pl.csanecki.animalshelter.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import pl.csanecki.animalshelter.service.AnimalRepository;
 
 public class AnimalRepositoryImpl implements AnimalRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public AnimalRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }

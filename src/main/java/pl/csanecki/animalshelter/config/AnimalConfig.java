a93c01fe4b25e97779a4373c9f3825fda0c09cf1
Cezary Sanecki <devcezz@gmail.com>
2020-11-21 00:20:46 +0100

    Registered bean od animal repository

--- src/main/java/pl/csanecki/animalshelter/config/AnimalConfig.java
+++ src/main/java/pl/csanecki/animalshelter/config/AnimalConfig.java
@@ -3,6 +3,8 @@ package pl.csanecki.animalshelter.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import pl.csanecki.animalshelter.repository.AnimalRepositoryImpl;
+import pl.csanecki.animalshelter.service.AnimalRepository;
 
 import javax.sql.DataSource;
 
@@ -12,5 +14,10 @@ public class AnimalConfig {
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public AnimalRepository animalRepository(JdbcTemplate jdbcTemplate) {
+        return new AnimalRepositoryImpl(jdbcTemplate);
     }
 }

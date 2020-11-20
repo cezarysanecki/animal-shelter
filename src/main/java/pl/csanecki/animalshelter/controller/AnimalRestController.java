48e1031148421055fcb02886941951e0f186dbd2
Cezary Sanecki <devcezz@gmail.com>
2020-11-20 23:53:05 +0100

    Added interface for animal service

--- src/main/java/pl/csanecki/animalshelter/controller/AnimalRestController.java
+++ src/main/java/pl/csanecki/animalshelter/controller/AnimalRestController.java
@@ -1,9 +1,17 @@
 package pl.csanecki.animalshelter.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/animals")
 public class AnimalRestController {
+
+    private AnimalSerivce animalSerivce;
+
+    @Autowired
+    public AnimalRestController(AnimalSerivce animalSerivce) {
+        this.animalSerivce = animalSerivce;
+    }
 }
---
+++ src/main/java/pl/csanecki/animalshelter/controller/AnimalSerivce.java
@@ -0,0 +1,4 @@
+package pl.csanecki.animalshelter.controller;
+
+public interface AnimalSerivce {
+}

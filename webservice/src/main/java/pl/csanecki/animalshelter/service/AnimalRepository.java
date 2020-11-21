acbd9762b6340ef3e172fe9cd613aaa5d0f9eef3
Cezary Sanecki <devcezz@gmail.com>
2020-11-21 01:04:47 +0100

    Added method for saving animal in db

--- webservice/src/main/java/pl/csanecki/animalshelter/service/AnimalRepository.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/service/AnimalRepository.java
@@ -1,4 +1,8 @@
 package pl.csanecki.animalshelter.service;
 
+import pl.csanecki.animalshelter.dto.AnimalCreated;
+import pl.csanecki.animalshelter.dto.AnimalRequest;
+
 public interface AnimalRepository {
+    AnimalCreated save(AnimalRequest animal);
 }

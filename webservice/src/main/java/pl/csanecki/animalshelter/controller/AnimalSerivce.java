fde5070a30fcbea7c4bfa5c52a234048192b6842
Cezary Sanecki <devcezz@gmail.com>
2020-11-21 01:00:46 +0100

    Added method for accepting animal in service

--- webservice/src/main/java/pl/csanecki/animalshelter/controller/AnimalSerivce.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/controller/AnimalSerivce.java
@@ -1,4 +1,8 @@
 package pl.csanecki.animalshelter.controller;
 
+import pl.csanecki.animalshelter.dto.AnimalCreated;
+import pl.csanecki.animalshelter.dto.AnimalRequest;
+
 public interface AnimalSerivce {
+    AnimalCreated accept(AnimalRequest animal);
 }

ede0ed48b54c25521dd3b89b1d94baee55980c9d
Cezary Sanecki <devcezz@gmail.com>
2020-11-21 01:00:05 +0100

    Added mapping for accepting animal into shelter

--- webservice/src/main/java/pl/csanecki/animalshelter/controller/AnimalRestController.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/controller/AnimalRestController.java
@@ -1,8 +1,12 @@
 package pl.csanecki.animalshelter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.csanecki.animalshelter.dto.AnimalCreated;
+import pl.csanecki.animalshelter.dto.AnimalRequest;
 
 @RestController
 @RequestMapping("/animals")
@@ -13,5 +17,12 @@ public class AnimalRestController {
     @Autowired
     public AnimalRestController(AnimalSerivce animalSerivce) {
         this.animalSerivce = animalSerivce;
+    }
+
+    @PostMapping
+    public ResponseEntity<AnimalCreated> acceptIntoShelter(AnimalRequest animal) {
+        animalSerivce.accept(animal);
+
+        throw new UnsupportedOperationException();
     }
 }

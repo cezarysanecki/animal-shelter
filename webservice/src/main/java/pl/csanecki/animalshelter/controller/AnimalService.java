e009eb9291189f8c620fce1899b7ea90d97b508c
Cezary Sanecki <devcezz@gmail.com>
2020-11-22 19:19:57 +0100

    Added method to service to get animal by id

--- webservice/src/main/java/pl/csanecki/animalshelter/controller/AnimalService.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/controller/AnimalService.java
@@ -1,8 +1,11 @@
 package pl.csanecki.animalshelter.controller;
 
 import pl.csanecki.animalshelter.dto.AnimalCreated;
+import pl.csanecki.animalshelter.dto.AnimalDetails;
 import pl.csanecki.animalshelter.dto.AnimalRequest;
 
 public interface AnimalService {
     AnimalCreated accept(AnimalRequest animal);
+
+    AnimalDetails getAnimalBy(int id);
 }

1d92270388293367798f3239f6ba60f4171aa8ca
Cezary Sanecki <devcezz@gmail.com>
2020-11-22 19:19:43 +0100

    Added endpoint to get animal by id

--- webservice/src/main/java/pl/csanecki/animalshelter/controller/AnimalRestController.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/controller/AnimalRestController.java
@@ -2,11 +2,9 @@ package pl.csanecki.animalshelter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.csanecki.animalshelter.dto.AnimalCreated;
+import pl.csanecki.animalshelter.dto.AnimalDetails;
 import pl.csanecki.animalshelter.dto.AnimalRequest;
 
 @RestController
@@ -23,6 +21,13 @@ public class AnimalRestController {
     @PostMapping
     public ResponseEntity<AnimalCreated> acceptIntoShelter(@RequestBody AnimalRequest animal) {
         animalService.accept(animal);
+
+        throw new UnsupportedOperationException();
+    }
+
+    @GetMapping("/:id")
+    public ResponseEntity<AnimalDetails> getAnimalDetails(@PathVariable int id) {
+        animalService.getAnimalBy(id);
 
         throw new UnsupportedOperationException();
     }

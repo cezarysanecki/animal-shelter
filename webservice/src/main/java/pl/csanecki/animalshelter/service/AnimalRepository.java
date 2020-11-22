a5bc7296cb88644f48d593d243fb25f0cd1ae32c
Cezary Sanecki <devcezz@gmail.com>
2020-11-22 19:42:42 +0100

    Changed returned type of creating animal in repo

--- webservice/src/main/java/pl/csanecki/animalshelter/service/AnimalRepository.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/service/AnimalRepository.java
@@ -1,12 +1,11 @@
 package pl.csanecki.animalshelter.service;
 
 import io.vavr.control.Option;
-import pl.csanecki.animalshelter.dto.AnimalCreated;
 import pl.csanecki.animalshelter.dto.AnimalDetails;
 import pl.csanecki.animalshelter.dto.AnimalRequest;
 
 public interface AnimalRepository {
-    AnimalCreated save(AnimalRequest animal);
+    Option<AnimalDetails> save(AnimalRequest animal);
 
     Option<AnimalDetails> findAnimalBy(int id);
 }

c56b02951cb915e15e9a28aa06250fbc5918850a
Cezary Sanecki <devcezz@gmail.com>
2020-11-22 19:33:32 +0100

    Changed signature to return Option of animal details

--- webservice/src/main/java/pl/csanecki/animalshelter/controller/AnimalService.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/controller/AnimalService.java
@@ -1,5 +1,6 @@
 package pl.csanecki.animalshelter.controller;
 
+import io.vavr.control.Option;
 import pl.csanecki.animalshelter.dto.AnimalCreated;
 import pl.csanecki.animalshelter.dto.AnimalDetails;
 import pl.csanecki.animalshelter.dto.AnimalRequest;
@@ -7,5 +8,5 @@ import pl.csanecki.animalshelter.dto.AnimalRequest;
 public interface AnimalService {
     AnimalCreated accept(AnimalRequest animal);
 
-    AnimalDetails getAnimalBy(int id);
+    Option<AnimalDetails> getAnimalBy(int id);
 }

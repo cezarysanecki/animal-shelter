8ab412e6aabda85c50abf15e4d2aa1b42d5505b1
Cezary Sanecki <devcezz@gmail.com>
2020-12-04 23:04:04 +0100

    Deleted unnecessary bean od JdbcTemplate

--- webservice/src/main/java/pl/csanecki/animalshelter/config/AnimalConfig.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/config/AnimalConfig.java
@@ -8,15 +8,8 @@ import pl.csanecki.animalshelter.repository.AnimalRepositoryImpl;
 import pl.csanecki.animalshelter.service.AnimalRepository;
 import pl.csanecki.animalshelter.service.AnimalServiceImpl;
 
-import javax.sql.DataSource;
-
 @Configuration(proxyBeanMethods = false)
 public class AnimalConfig {
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
-    }
 
     @Bean
     public AnimalRepository animalRepository(JdbcTemplate jdbcTemplate) {

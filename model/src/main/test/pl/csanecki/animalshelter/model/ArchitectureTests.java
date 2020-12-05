79359ecc485deba4928d2e7f70e96b9d1418f65d
Cezary Sanecki <devcezz@gmail.com>
2020-12-05 17:43:52 +0100

    Added test for protecting model module from webservices classes

---
+++ model/src/main/test/pl/csanecki/animalshelter/model/ArchitectureTests.java
@@ -0,0 +1,19 @@
+package pl.csanecki.animalshelter.model;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+@AnalyzeClasses(packagesOf = ArchitectureTests.class)
+public class ArchitectureTests {
+
+    @ArchTest
+    public static final ArchRule should_not_contain_any_webservice_references_in_model = noClasses()
+            .that(resideInAPackage("..pl.csanecki.animalshelter.model.."))
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("..pl.csanecki.animalshelter.webservice..");
+}

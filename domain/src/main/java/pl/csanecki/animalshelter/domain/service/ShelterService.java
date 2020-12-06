5bf4301a0233e3940afe3b7221b284ed2d4ce8ee
Cezary Sanecki <devcezz@gmail.com>
2020-12-06 23:44:28 +0100

    Made shelter service compile - in working

--- domain/src/main/java/pl/csanecki/animalshelter/domain/service/ShelterService.java
+++ domain/src/main/java/pl/csanecki/animalshelter/domain/service/ShelterService.java
@@ -3,10 +3,18 @@ package pl.csanecki.animalshelter.domain.service;
 import io.vavr.collection.List;
 import io.vavr.control.Option;
 import pl.csanecki.animalshelter.domain.animal.AddAnimalCommand;
-import pl.csanecki.animalshelter.domain.animal.model.AnimalId;
 import pl.csanecki.animalshelter.domain.service.entity.AnimalData;
-import pl.csanecki.animalshelter.domain.service.entity.AnimalInformation;
+import pl.csanecki.animalshelter.domain.service.entity.AnimalDescription;
+import pl.csanecki.animalshelter.domain.service.entity.AnimalId;
+import pl.csanecki.animalshelter.domain.validation.FailedValidationResult;
+import pl.csanecki.animalshelter.domain.validation.SucceededValidationResult;
+import pl.csanecki.animalshelter.domain.validation.ValidationResult;
 import pl.csanecki.animalshelter.domain.validation.Validator;
+
+import java.util.Set;
+
+import static io.vavr.API.*;
+import static io.vavr.Predicates.instanceOf;
 
 public class ShelterService {
 
@@ -19,14 +27,25 @@ public class ShelterService {
     }
 
     public Option<AnimalData> accept(AddAnimalCommand command) {
-        return animalRepository.save(new AnimalInformation(
+        ValidationResult validationResult = validator.validate(Set.of(
                 command.getAnimalName(),
                 command.getAnimalKind(),
                 command.getAnimalAge()
         ));
+
+        Match(validationResult).of(
+                Case($(instanceOf(SucceededValidationResult.class)), save(command)),
+                Case($(instanceOf(FailedValidationResult.class)), save(command))
+        );
+
+        return Option.none();
     }
 
-    public Option<AnimalData> getAnimalBy(int id) {
+    private Option<AnimalData> save(AddAnimalCommand command) {
+        return animalRepository.save(AnimalDescription.of(command.getAnimalName(), command.getAnimalKind(), command.getAnimalAge()));
+    }
+
+    public Option<AnimalData> getAnimalBy(long id) {
         return animalRepository.findAnimalBy(new AnimalId(id));
     }
 

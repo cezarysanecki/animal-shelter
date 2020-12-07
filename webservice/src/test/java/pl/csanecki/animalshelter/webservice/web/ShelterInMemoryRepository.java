1b11dc776faf3cbff0c1d638c12713bd494eb9df
Cezary Sanecki <devcezz@gmail.com>
2020-12-07 23:51:48 +0100

    Added implementation of in memory repository

---
+++ webservice/src/test/java/pl/csanecki/animalshelter/webservice/web/ShelterInMemoryRepository.java
@@ -0,0 +1,7 @@
+package pl.csanecki.animalshelter.webservice.web;
+
+import pl.csanecki.animalshelter.domain.service.ShelterRepository;
+
+public class ShelterInMemoryRepository implements ShelterRepository {
+
+}

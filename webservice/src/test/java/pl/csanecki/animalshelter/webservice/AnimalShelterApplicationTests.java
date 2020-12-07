c078c1c16f7517a72d130184e8ae18b95d929286
Cezary Sanecki <devcezz@gmail.com>
2020-12-07 21:09:44 +0100

    Added active profile dev for context load test

--- webservice/src/test/java/pl/csanecki/animalshelter/webservice/AnimalShelterApplicationTests.java
+++ webservice/src/test/java/pl/csanecki/animalshelter/webservice/AnimalShelterApplicationTests.java
@@ -2,12 +2,12 @@ package pl.csanecki.animalshelter.webservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class AnimalShelterApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() { }
 }

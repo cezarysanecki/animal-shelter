404c7b0a64f66f426b7f6612e854e7cf560ee359
Cezary Sanecki <devcezz@gmail.com>
2020-12-08 17:24:00 +0100

    Added implementation for saving animal in database

--- webservice/src/main/java/pl/csanecki/animalshelter/webservice/repository/ShelterJdbcRepository.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/webservice/repository/ShelterJdbcRepository.java
@@ -1,9 +1,16 @@
 package pl.csanecki.animalshelter.webservice.repository;
 
+import io.vavr.control.Option;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.transaction.annotation.Transactional;
 import pl.csanecki.animalshelter.domain.command.AddAnimalCommand;
 import pl.csanecki.animalshelter.domain.model.AnimalId;
 import pl.csanecki.animalshelter.domain.service.ShelterRepository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class ShelterJdbcRepository implements ShelterRepository {
 
@@ -14,7 +21,23 @@ public class ShelterJdbcRepository implements ShelterRepository {
     }
 
     @Override
+    @Transactional
     public AnimalId registerAnimal(AddAnimalCommand command) {
-        return null;
+        KeyHolder holder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "INSERT INTO animals(name, kind, age) VALUES(?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            preparedStatement.setString(1, command.getAnimalName());
+            preparedStatement.setString(2, command.getAnimalKind());
+            preparedStatement.setInt(3, command.getAnimalAge());
+            return preparedStatement;
+        }, holder);
+
+        return Option.of(holder.getKey())
+                .map(key -> AnimalId.of(key.longValue()))
+                .getOrElseThrow(() -> { throw new DatabaseRuntimeError("Cannot get id for admitted animal"); });
     }
 }

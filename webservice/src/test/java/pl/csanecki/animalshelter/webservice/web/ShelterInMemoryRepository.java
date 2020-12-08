3e20ebc5a820d20bb29561eafa9f9a95dfba9671
Cezary Sanecki <devcezz@gmail.com>
2020-12-08 16:00:24 +0100

    Deleted unnecessary in memory database for tests

--- webservice/src/test/java/pl/csanecki/animalshelter/webservice/web/ShelterInMemoryRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.csanecki.animalshelter.webservice.web;
-
-import pl.csanecki.animalshelter.domain.service.ShelterRepository;
-
-public class ShelterInMemoryRepository implements ShelterRepository {
-
-}

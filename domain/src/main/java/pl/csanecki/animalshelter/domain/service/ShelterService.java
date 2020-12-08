10ab5ff7df627e79b15addc32dfe62b9a2b5708a
Cezary Sanecki <devcezz@gmail.com>
2020-12-08 14:31:00 +0100

    Handled method to accept animal into shelter

--- domain/src/main/java/pl/csanecki/animalshelter/domain/service/ShelterService.java
+++ domain/src/main/java/pl/csanecki/animalshelter/domain/service/ShelterService.java
@@ -1,10 +1,17 @@
 package pl.csanecki.animalshelter.domain.service;
 
+import pl.csanecki.animalshelter.domain.command.AddAnimalCommand;
+import pl.csanecki.animalshelter.domain.model.AnimalId;
+
 public class ShelterService {
 
     private final ShelterRepository shelterRepository;
 
     public ShelterService(ShelterRepository shelterRepository) {
         this.shelterRepository = shelterRepository;
+    }
+
+    public AnimalId acceptIntoShelter(AddAnimalCommand command) {
+        return shelterRepository.registerAnimal(command);
     }
 }

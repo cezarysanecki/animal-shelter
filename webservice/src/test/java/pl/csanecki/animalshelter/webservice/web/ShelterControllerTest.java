79f67784c551b5902d89c7c4c96178777d5d88e2
Cezary Sanecki <devcezz@gmail.com>
2020-12-08 16:02:06 +0100

    Changed conception for http tests
    Used mock for shelter service
    Verified location for response of admitting animal

--- webservice/src/test/java/pl/csanecki/animalshelter/webservice/web/ShelterControllerTest.java
+++ webservice/src/test/java/pl/csanecki/animalshelter/webservice/web/ShelterControllerTest.java
@@ -1,32 +1,49 @@
 package pl.csanecki.animalshelter.webservice.web;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import pl.csanecki.animalshelter.domain.model.AnimalId;
 import pl.csanecki.animalshelter.domain.service.ShelterService;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static pl.csanecki.animalshelter.webservice.model.AnimalFixture.anyAnimalId;
 
-@WebMvcTest
+@RunWith(SpringRunner.class)
+@WebMvcTest(ShelterController.class)
 @ContextConfiguration(classes = MockMvcConfig.class)
 class ShelterControllerTest {
 
+    AnimalId animalId = anyAnimalId();
+
     @Test
-    void should_admit_animal_to_shelter(@Autowired MockMvc mockMvc) throws Exception {
+    void should_admit_animal_to_shelter(@Autowired MockMvc mockMvc, @Autowired ShelterService shelterService) throws Exception {
+        given(shelterService.acceptIntoShelter(any())).willReturn(animalId);
+
         mockMvc.perform(post("/shelter/animals")
                 .content(animalToAdmit())
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.LOCATION, "/shelter/animals/" + animalId.getAnimalId()));
     }
 
     private String animalToAdmit() throws IOException {
@@ -39,7 +56,7 @@ class MockMvcConfig {
 
     @Bean
     ShelterService fakeShelterService() {
-        return new ShelterService(new ShelterInMemoryRepository());
+        return mock(ShelterService.class);
     }
 
     @Bean

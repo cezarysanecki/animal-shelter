a38f35cd32950602e957c67ea8ad2ed4e75ef8b4
Cezary Sanecki <devcezz@gmail.com>
2020-12-09 18:53:27 +0100

    Added method to get animal details in repository

--- domain/src/main/java/pl/csanecki/animalshelter/domain/service/ShelterRepository.java
+++ domain/src/main/java/pl/csanecki/animalshelter/domain/service/ShelterRepository.java
@@ -1,9 +1,13 @@
 package pl.csanecki.animalshelter.domain.service;
 
+import io.vavr.control.Option;
+import pl.csanecki.animalshelter.domain.animal.AnimalDetails;
 import pl.csanecki.animalshelter.domain.command.AddAnimalCommand;
 import pl.csanecki.animalshelter.domain.model.AnimalId;
 
 public interface ShelterRepository {
 
     AnimalId registerAnimal(AddAnimalCommand command);
+
+    Option<AnimalDetails> getAnimalDetails(AnimalId animalId);
 }

712146f91880d4d9841eacab176aee646c0df42c
Cezary Sanecki <devcezz@gmail.com>
2020-12-09 20:11:48 +0100

    Added one element list of animals

--- webservice/src/test/java/pl/csanecki/animalshelter/webservice/model/AnimalFixture.java
+++ webservice/src/test/java/pl/csanecki/animalshelter/webservice/model/AnimalFixture.java
@@ -1,12 +1,14 @@
 package pl.csanecki.animalshelter.webservice.model;
 
 import pl.csanecki.animalshelter.domain.animal.AnimalDetails;
+import pl.csanecki.animalshelter.domain.animal.AnimalShortInfo;
 import pl.csanecki.animalshelter.domain.model.AnimalAge;
 import pl.csanecki.animalshelter.domain.model.AnimalId;
 import pl.csanecki.animalshelter.domain.model.AnimalKind;
 import pl.csanecki.animalshelter.domain.model.AnimalName;
 
 import java.time.Instant;
+import java.util.List;
 
 public class AnimalFixture {
 
@@ -20,5 +22,9 @@ public class AnimalFixture {
 
     public static AnimalDetails animalInShelter(long id) {
         return new AnimalDetails(animalId(id), AnimalName.of("Azor"), AnimalKind.of("DOG"), AnimalAge.of(2), Instant.now(), null);
+    }
+
+    public static List<AnimalShortInfo> animalsInShelter() {
+        return List.of(new AnimalShortInfo(anyAnimalId(), AnimalName.of("Azor"), AnimalKind.of("DOG"), AnimalAge.of(2), true));
     }
 }

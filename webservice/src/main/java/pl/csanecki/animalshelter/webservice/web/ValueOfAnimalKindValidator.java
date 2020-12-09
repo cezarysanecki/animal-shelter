61479ffe08547435f32c4cd4e00880509aa97aec
Cezary Sanecki <devcezz@gmail.com>
2020-12-10 00:43:47 +0100

    Added message in validation for animal kind

--- webservice/src/main/java/pl/csanecki/animalshelter/webservice/web/ValueOfAnimalKindValidator.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/webservice/web/ValueOfAnimalKindValidator.java
@@ -9,16 +9,26 @@ import javax.validation.ConstraintValidatorContext;
 public class ValueOfAnimalKindValidator implements ConstraintValidator<ValueOfAnimalKind, CharSequence> {
 
     private List<String> acceptedValues;
+    private String message;
 
     @Override
     public void initialize(ValueOfAnimalKind constraintAnnotation) {
         acceptedValues = Stream.of(constraintAnnotation.enumClass().getEnumConstants())
                 .map(Enum::name)
                 .toList();
+
+        message = String.format("{javax.validation.constraints.ValueOfAnimalKind.message}: %s", acceptedValues.mkString(", "));
     }
 
     @Override
     public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
-        return value != null && acceptedValues.contains(value.toString());
+        if (value != null && acceptedValues.contains(value.toString())) {
+            return true;
+        }
+
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(message).addConstraintViolation();
+
+        return false;
     }
 }

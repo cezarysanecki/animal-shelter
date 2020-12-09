aa9b002ed316bd5f2cf7c2d690353f80535762f4
Cezary Sanecki <devcezz@gmail.com>
2020-12-09 18:52:07 +0100

    Added fixture for animal details

--- webservice/src/test/java/pl/csanecki/animalshelter/webservice/model/AnimalFixture.java
+++ webservice/src/test/java/pl/csanecki/animalshelter/webservice/model/AnimalFixture.java
@@ -1,6 +1,12 @@
 package pl.csanecki.animalshelter.webservice.model;
 
+import pl.csanecki.animalshelter.domain.animal.AnimalDetails;
+import pl.csanecki.animalshelter.domain.model.AnimalAge;
 import pl.csanecki.animalshelter.domain.model.AnimalId;
+import pl.csanecki.animalshelter.domain.model.AnimalKind;
+import pl.csanecki.animalshelter.domain.model.AnimalName;
+
+import java.time.Instant;
 
 public class AnimalFixture {
 
@@ -10,5 +16,9 @@ public class AnimalFixture {
 
     public static AnimalId animalId(long id) {
         return AnimalId.of(id);
+    }
+
+    public static AnimalDetails animalInShelter(long id) {
+        return new AnimalDetails(animalId(id), AnimalName.of("Azor"), AnimalKind.of("DOG"), AnimalAge.of(2), Instant.now(), null);
     }
 }

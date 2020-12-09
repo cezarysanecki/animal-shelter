b6af4cd3d917c40598f45ca8e5815d56db77939a
Cezary Sanecki <devcezz@gmail.com>
2020-12-10 00:43:21 +0100

    Changed message for enum animal kind annotation

--- webservice/src/main/java/pl/csanecki/animalshelter/webservice/web/ValueOfAnimalKind.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/webservice/web/ValueOfAnimalKind.java
@@ -15,7 +15,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Constraint(validatedBy = ValueOfAnimalKindValidator.class)
 public @interface ValueOfAnimalKind {
     Class<? extends Enum<?>> enumClass();
-    String message() default "must be any of enum {enumClass}";
+    String message() default "{javax.validation.constraints.ValueOfAnimalKind.message}";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }

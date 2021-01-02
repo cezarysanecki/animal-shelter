f9319c72d4fe9bcc82c12d9f5a0c556f8eeb4f9a
Cezary Sanecki <devcezz@gmail.com>
2021-01-02 19:46:15 +0100

    Exported navbar component in core module

--- webservice/src/main/web/src/app/core/core.module.ts
+++ webservice/src/main/web/src/app/core/core.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 
-
-
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    NavbarComponent
   ]
 })
 export class CoreModule { }

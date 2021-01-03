450c5bb81999d627077caff5930109ec8db61166
Cezary Sanecki <devcezz@gmail.com>
2021-01-03 17:18:34 +0100

    Registered animal card component

--- webservice/src/main/web/src/app/animals/animals.module.ts
+++ webservice/src/main/web/src/app/animals/animals.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnimalsBrowserComponent } from './animals-browser/animals-browser.component';
+import {AnimalsBrowserCardComponent} from "./animals-browser/components/animals-browser-card/animals-browser-card.component";
 
 @NgModule({
-  declarations: [AnimalsBrowserComponent],
+  declarations: [AnimalsBrowserComponent, AnimalsBrowserCardComponent],
   exports: [
     AnimalsBrowserComponent
   ],

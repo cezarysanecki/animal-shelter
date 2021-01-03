470b2e75ef7aadc3791c4ea4d70849e9469165a2
Cezary Sanecki <devcezz@gmail.com>
2021-01-03 19:00:49 +0100

    Import animals browser module to animals module

--- webservice/src/main/web/src/app/animals/animals.module.ts
+++ webservice/src/main/web/src/app/animals/animals.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AnimalsBrowserComponent } from './views/animals-browser/animals-browser.component';
-import {AnimalsBrowserCardComponent} from "./views/animals-browser/components/animals-browser-card/animals-browser-card.component";
+import {AnimalsBrowserModule} from "./views/animals-browser/animals-browser.module";
 
 @NgModule({
-  declarations: [AnimalsBrowserComponent, AnimalsBrowserCardComponent],
-  exports: [
-    AnimalsBrowserComponent
-  ],
+  declarations: [],
+  exports: [],
   imports: [
-    CommonModule
+    CommonModule,
+    AnimalsBrowserModule
   ]
 })
 export class AnimalsModule { }

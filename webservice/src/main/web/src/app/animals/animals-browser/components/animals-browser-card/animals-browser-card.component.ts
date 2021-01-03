1b6f46dfc0f8f91e097535da739a1c0a8c42c9af
Cezary Sanecki <devcezz@gmail.com>
2021-01-03 12:07:12 +0100

    Added component to extract animal card

---
+++ webservice/src/main/web/src/app/animals/animals-browser/components/animals-browser-card/animals-browser-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animals-browser-card',
+  templateUrl: './animals-browser-card.component.html',
+  styleUrls: ['./animals-browser-card.component.css']
+})
+export class AnimalsBrowserCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

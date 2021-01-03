940bf8627d4a74c8ab8057a29bfcbe45d9d42a2b
Cezary Sanecki <devcezz@gmail.com>
2021-01-03 02:35:13 +0100

    Added style for browsing animals

--- webservice/src/main/web/src/app/animals/animals-browser/animals-browser.component.ts
+++ webservice/src/main/web/src/app/animals/animals-browser/animals-browser.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-animals-browser',
-  templateUrl: './animals-browser.component.html'
+  templateUrl: './animals-browser.component.html',
+  styleUrls: ['./animals-browser.component.css']
 })
 export class AnimalsBrowserComponent implements OnInit {
 

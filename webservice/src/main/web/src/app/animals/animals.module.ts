b82c493ce0a6110ab2f0be878e9bd5a59c39cb5e
Cezary Sanecki <devcezz@gmail.com>
2021-01-03 17:21:59 +0100

    Moved animals browser to views folder

--- webservice/src/main/web/src/app/animals/animals.module.ts
+++ webservice/src/main/web/src/app/animals/animals.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AnimalsBrowserComponent } from './animals-browser/animals-browser.component';
-import {AnimalsBrowserCardComponent} from "./animals-browser/components/animals-browser-card/animals-browser-card.component";
+import { AnimalsBrowserComponent } from './views/animals-browser/animals-browser.component';
+import {AnimalsBrowserCardComponent} from "./views/animals-browser/components/animals-browser-card/animals-browser-card.component";
 
 @NgModule({
   declarations: [AnimalsBrowserComponent, AnimalsBrowserCardComponent],

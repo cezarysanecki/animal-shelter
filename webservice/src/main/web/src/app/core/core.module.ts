9d169ef82e4f179316165fee9cf6149dfa9cf8f9
Cezary Sanecki <devcezz@gmail.com>
2021-01-03 18:50:47 +0100

    Added config in app module

--- webservice/src/main/web/src/app/core/core.module.ts
+++ webservice/src/main/web/src/app/core/core.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import {Config} from "./config/config";
+
+const config: Config = {
+  apiUrl: 'http://localhost:8080'
+}
 
 @NgModule({
   declarations: [NavbarComponent],

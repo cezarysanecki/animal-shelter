b466796b1faaef93c52e57ef70cbfa26a2d5b705
Cezary Sanecki <devcezz@gmail.com>
2021-01-03 01:47:22 +0100

    Added component of animals browser

---
+++ webservice/src/main/web/src/app/animals/animals-browser/animals-browser.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animals-browser',
+  templateUrl: './animals-browser.component.html'
+})
+export class AnimalsBrowserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ webservice/src/main/web/src/app/animals/animals.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AnimalsBrowserComponent } from './animals-browser/animals-browser.component';
+
+@NgModule({
+  declarations: [AnimalsBrowserComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AnimalsModule { }

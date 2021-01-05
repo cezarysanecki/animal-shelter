36c52da5c509070249efbadd88e1bd761c23375c
Cezary Sanecki <devcezz@gmail.com>
2021-01-05 21:57:25 +0100

    Moved animal details component

--- webservice/src/main/web/src/app/animals/views/animals-browser/components/animal-details/animal-details.component.ts
+++ webservice/src/main/web/src/app/animals/views/animals-browser/components/animal-details/animal-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-animal-details',
@@ -11,5 +11,7 @@ export class AnimalDetailsComponent implements OnInit {
 
   ngOnInit() { }
 
-  close() { }
+  close() {
+
+  }
 }

42cc07b440769fa45b28f5363f985105e7ff5fc1
Cezary Sanecki <devcezz@gmail.com>
2021-01-05 21:57:50 +0100

    Deleted module of animal details from app module

--- webservice/src/main/web/src/app/animals/animals.module.ts
+++ webservice/src/main/web/src/app/animals/animals.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnimalsBrowserModule } from "@animals/views/animals-browser";
-import { AnimalDetailsComponent } from './views/animal-details/animal-details.component';
 
 @NgModule({
-  declarations: [AnimalDetailsComponent],
+  declarations: [],
   exports: [
-    AnimalsBrowserModule,
-    AnimalDetailsComponent
+    AnimalsBrowserModule
   ],
   imports: [
     CommonModule,

4ed49ba3946659931f07c336a61ba5510a245849
Cezary Sanecki <devcezz@gmail.com>
2021-01-05 17:25:44 +0100

    Added component of animal details

---
+++ webservice/src/main/web/src/app/animals/views/animal-details/animal-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-details',
+  templateUrl: './animal-details.component.html',
+  styleUrls: ['./animal-details.component.css']
+})
+export class AnimalDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

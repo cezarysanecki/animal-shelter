1773af966cc2b73c0beb35e4ef736cfff781beb0
Cezary Sanecki <devcezz@gmail.com>
2021-01-06 13:56:53 +0100

    Added animation for loader

--- webservice/src/main/web/src/app/shared/components/app-loader/app-loader.component.ts
+++ webservice/src/main/web/src/app/shared/components/app-loader/app-loader.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from "rxjs";
 import { AppLoaderService } from "./app-loader.service";
+import { animate, style, transition, trigger } from "@angular/animations";
 
 @Component({
   selector: 'app-app-loader',
   templateUrl: './app-loader.component.html',
-  styleUrls: ['./app-loader.component.css']
+  styleUrls: ['./app-loader.component.css'],
+  animations: [
+    trigger('loader', [
+      transition(':enter', [
+        style({ opacity: '0' }),
+        animate(300)
+      ]),
+      transition(':leave', [
+        animate(300, style({ opacity: '0' }))
+      ])
+    ])
+  ]
 })
 export class AppLoaderComponent implements OnDestroy {
 

0784088855a675ad64de0ccc9b525f7f2902d7dd
Cezary Sanecki <devcezz@gmail.com>
2021-01-07 13:38:59 +0100

    Created method mappers

--- webservice/src/main/web/src/app/animals/views/animals-browser/animals-browser.service.ts
+++ webservice/src/main/web/src/app/animals/views/animals-browser/animals-browser.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-import {AnimalsHttpService} from "@animals/services";
+import { AnimalsHttpService } from "@animals/services";
+import { map } from "rxjs/operators";
+import { AnimalKind } from "@animals/enums";
+import { GetAnimalDetails, GetAnimalsRecord } from "@animals/services/types/http-animals.type";
+import { AnimalDetails, AnimalShortInfo } from "@animals/types/animals.type";
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +15,38 @@ export class AnimalsBrowserService {
   ) { }
 
   getAnimals() {
-    return this.animalsHttpService.getAnimals();
+    return this.animalsHttpService.getAnimals().pipe(
+      map(result => result.animals),
+      map(animals => animals.map(animal => AnimalsBrowserService.convertToAnimalShortInfo(animal)))
+    );
   }
 
   getAnimalDetails(animalId: number) {
-    return this.animalsHttpService.getAnimalDetails(animalId);
+    return this.animalsHttpService.getAnimalDetails(animalId).pipe(
+      map(animal => AnimalsBrowserService.convertToAnimalDetails(animal))
+    );
+  }
+
+  private static convertToAnimalShortInfo(animalRecord: GetAnimalsRecord): AnimalShortInfo {
+    return {
+      id: animalRecord.id,
+      name: animalRecord.name,
+      age: animalRecord.age,
+      imageUrl: `assets/images/animal-kinds/${AnimalKind[animalRecord.kind]}.jpg`,
+      translocoKind: `animal-kind.${AnimalKind[animalRecord.kind]}`,
+      inShelter: animalRecord.inShelter
+    }
+  }
+
+  private static convertToAnimalDetails(animalDetails: GetAnimalDetails): AnimalDetails {
+    return {
+      id: animalDetails.id,
+      name: animalDetails.name,
+      age: animalDetails.age,
+      imageUrl: `assets/images/animal-kinds/${AnimalKind[animalDetails.kind]}.jpg`,
+      translocoKind: `animal-kind.${AnimalKind[animalDetails.kind]}`,
+      admittedAt: animalDetails.admittedAt,
+      adoptedAt: animalDetails.adoptedAt,
+    }
   }
 }

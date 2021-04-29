6d6513bf5509e305e2ff246bebaab1efa89d402b
Cezary Sanecki <devcezz@gmail.com>
2021-04-29 14:08:13 +0200

    Delomboked AnimalShortInfo class

--- domain/src/main/java/pl/csanecki/animalshelter/domain/animal/AnimalShortInfo.java
+++ domain/src/main/java/pl/csanecki/animalshelter/domain/animal/AnimalShortInfo.java
@@ -1,18 +1,40 @@
 package pl.csanecki.animalshelter.domain.animal;
 
-import lombok.Value;
 import pl.csanecki.animalshelter.domain.model.AnimalKind;
 
-@Value
 public class AnimalShortInfo {
 
-    long id;
-    String name;
-    AnimalKind kind;
-    int age;
-    boolean inShelter;
+    private final long id;
+    private final String name;
+    private final AnimalKind kind;
+    private final int age;
+    private final boolean inShelter;
 
-    String getKind() {
-        return kind.name();
+    public AnimalShortInfo(final long id, final String name, final AnimalKind kind, final int age, final boolean inShelter) {
+        this.id = id;
+        this.name = name;
+        this.kind = kind;
+        this.age = age;
+        this.inShelter = inShelter;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public AnimalKind getKind() {
+        return kind;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isInShelter() {
+        return inShelter;
     }
 }

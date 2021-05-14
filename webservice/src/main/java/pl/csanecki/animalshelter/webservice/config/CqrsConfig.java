bb9407b00bbf3f4aeafab678d9cfe27f04ac06a3
Cezary Sanecki <devcezz@gmail.com>
2021-05-15 00:27:46 +0200

    Added configuration for cqrs buses

---
+++ webservice/src/main/java/pl/csanecki/animalshelter/webservice/config/CqrsConfig.java
@@ -0,0 +1,26 @@
+package pl.csanecki.animalshelter.webservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pl.devcezz.cqrs.command.AutoCommandsBus;
+import pl.devcezz.cqrs.command.CommandHandler;
+import pl.devcezz.cqrs.command.CommandsBus;
+import pl.devcezz.cqrs.event.AutoEventBus;
+import pl.devcezz.cqrs.event.EventHandler;
+import pl.devcezz.cqrs.event.EventsBus;
+
+import java.util.Set;
+
+@Configuration
+class CqrsConfig {
+
+    @Bean
+    CommandsBus commandsBus(Set<CommandHandler> handlers) {
+        return new AutoCommandsBus(handlers);
+    }
+
+    @Bean
+    EventsBus eventsBus(Set<EventHandler> handlers) {
+        return new AutoEventBus(handlers);
+    }
+}

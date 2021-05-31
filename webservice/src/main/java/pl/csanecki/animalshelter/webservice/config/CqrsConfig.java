c1eb1a52ef3f2569bb47533861ca4d836f932639
Cezary Sanecki <devcezz@gmail.com>
2021-05-31 16:45:38 +0200

    Deleted bean for event bus

--- webservice/src/main/java/pl/csanecki/animalshelter/webservice/config/CqrsConfig.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/webservice/config/CqrsConfig.java
@@ -8,9 +8,6 @@ import pl.csanecki.animalshelter.webservice.foo.adopt.AdoptAnimalCommandHandler;
 import pl.devcezz.cqrs.command.AutoCommandsBus;
 import pl.devcezz.cqrs.command.CommandHandler;
 import pl.devcezz.cqrs.command.CommandsBus;
-import pl.devcezz.cqrs.event.AutoEventsBus;
-import pl.devcezz.cqrs.event.EventHandler;
-import pl.devcezz.cqrs.event.EventsBus;
 
 import java.util.Set;
 
@@ -30,10 +27,5 @@ class CqrsConfig {
     @Bean
     CommandsBus commandsBus(Set<CommandHandler> handlers) {
         return new AutoCommandsBus(handlers);
-    }
-
-    @Bean
-    EventsBus eventsBus(Set<EventHandler> handlers) {
-        return new AutoEventsBus(handlers);
     }
 }

6f09800b68a136caf5b6f8c3a86d2c7e39c33203
Cezary Sanecki <devcezz@gmail.com>
2021-05-31 16:41:41 +0200

    Changed bean for write repository

--- webservice/src/main/java/pl/csanecki/animalshelter/webservice/config/ShelterAppConfig.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/webservice/config/ShelterAppConfig.java
@@ -3,20 +3,14 @@ package pl.csanecki.animalshelter.webservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import pl.csanecki.animalshelter.webservice.repository.ShelterJdbcRepository;
-import pl.csanecki.animalshelter.domain.service.ShelterRepository;
-import pl.csanecki.animalshelter.domain.service.ShelterService;
+import pl.csanecki.animalshelter.webservice.foo.ShelterJdbcWriteRepository;
+import pl.csanecki.animalshelter.webservice.foo.ShelterWriteRepository;
 
 @Configuration(proxyBeanMethods = false)
 public class ShelterAppConfig {
 
     @Bean
-    public ShelterRepository shelterRepository(JdbcTemplate jdbcTemplate) {
-        return new ShelterJdbcRepository(jdbcTemplate);
-    }
-
-    @Bean
-    public ShelterService shelterService(ShelterRepository shelterRepository) {
-        return new ShelterService(shelterRepository);
+    public ShelterWriteRepository shelterWriteRepository(JdbcTemplate jdbcTemplate) {
+        return new ShelterJdbcWriteRepository(jdbcTemplate);
     }
 }

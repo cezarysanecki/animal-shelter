6b28b123e42e42202ec7d4d2206961552927b043
Cezary Sanecki <devcezz@gmail.com>
2021-05-31 16:07:10 +0200

    Injected write repo to command handlers

--- webservice/src/main/java/pl/csanecki/animalshelter/webservice/foo/add/AddAnimalCommandHandler.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/webservice/foo/add/AddAnimalCommandHandler.java
@@ -1,8 +1,15 @@
 package pl.csanecki.animalshelter.webservice.foo.add;
 
+import pl.csanecki.animalshelter.webservice.foo.ShelterWriteRepository;
 import pl.devcezz.cqrs.command.CommandHandler;
 
 public class AddAnimalCommandHandler implements CommandHandler<AddAnimalCommand> {
+
+    private final ShelterWriteRepository repository;
+
+    AddAnimalCommandHandler(final ShelterWriteRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public void handle(final AddAnimalCommand command) {
--- webservice/src/main/java/pl/csanecki/animalshelter/webservice/foo/adopt/AdoptAnimalCommandHandler.java
+++ webservice/src/main/java/pl/csanecki/animalshelter/webservice/foo/adopt/AdoptAnimalCommandHandler.java
@@ -1,8 +1,15 @@
 package pl.csanecki.animalshelter.webservice.foo.adopt;
 
+import pl.csanecki.animalshelter.webservice.foo.ShelterWriteRepository;
 import pl.devcezz.cqrs.command.CommandHandler;
 
 public class AdoptAnimalCommandHandler implements CommandHandler<AdoptAnimalCommand> {
+
+    private final ShelterWriteRepository repository;
+
+    AdoptAnimalCommandHandler(final ShelterWriteRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public void handle(final AdoptAnimalCommand command) {

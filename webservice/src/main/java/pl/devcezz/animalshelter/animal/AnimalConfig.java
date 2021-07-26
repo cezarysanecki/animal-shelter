0d4117513c26bd4a8ee814c236e6ee1c9e56db06
Cezary Sanecki <devcezz@gmail.com>
2021-07-26 23:08:28 +0200

    Added event handlers

--- webservice/src/main/java/pl/devcezz/animalshelter/animal/AnimalConfig.java
+++ webservice/src/main/java/pl/devcezz/animalshelter/animal/AnimalConfig.java
@@ -3,6 +3,7 @@ package pl.devcezz.animalshelter.animal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import pl.devcezz.cqrs.event.EventHandler;
 import pl.devcezz.cqrs.event.EventsBus;
 
 @Configuration(proxyBeanMethods = false)
@@ -21,5 +22,20 @@ public class AnimalConfig {
     @Bean
     AcceptingAnimal acceptingAnimal(Animals animals, ShelterFactory shelterFactory) {
         return new AcceptingAnimal(animals, shelterFactory);
+    }
+
+    @Bean
+    EventHandler handleFailedAcceptance() {
+        return new HandleFailedAcceptance();
+    }
+
+    @Bean
+    EventHandler handleWarnedAcceptance() {
+        return new HandleWarnedAcceptance();
+    }
+
+    @Bean
+    EventHandler handleSucceededAcceptance() {
+        return new HandleSucceededAcceptance();
     }
 }

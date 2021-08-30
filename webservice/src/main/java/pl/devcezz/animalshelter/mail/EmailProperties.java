f105e32ade1d054b1853784c33402c8d29c4ffe9
Cezary Sanecki <devcezz@gmail.com>
2021-08-30 21:17:23 +0200

    Repaired property binding

--- webservice/src/main/java/pl/devcezz/animalshelter/mail/EmailProperties.java
+++ webservice/src/main/java/pl/devcezz/animalshelter/mail/EmailProperties.java
@@ -2,12 +2,10 @@ package pl.devcezz.animalshelter.mail;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
-import org.springframework.context.annotation.PropertySource;
 
-@PropertySource(value = "classpath:/mail-${spring.profiles.active}.yaml")
 @ConstructorBinding
 @ConfigurationProperties(prefix = "mail")
-public record EmailProperties(EmailServer server, EmailCredentials credentials, EmailSettings settings) {}
+record EmailProperties(EmailServer server, EmailCredentials credentials, EmailSettings settings) {}
 
 record EmailServer(String host, Integer port) {}
 

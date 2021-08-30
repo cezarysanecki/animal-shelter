911694096c89c7bbe4bd5c5be637025d3279707f
Cezary Sanecki <devcezz@gmail.com>
2021-08-30 17:14:30 +0200

    Changed handler for adopted animal event

--- application/src/main/java/pl/devcezz/animalshelter/animal/HandleAdoptingAnimal.java
+++ application/src/main/java/pl/devcezz/animalshelter/animal/HandleAdoptingAnimal.java
@@ -1,12 +1,32 @@
 package pl.devcezz.animalshelter.animal;
 
 import pl.devcezz.animalshelter.animal.event.AnimalEvent.AnimalAdoptionSucceeded;
+import pl.devcezz.animalshelter.commons.exception.NotFoundAnimalInShelterException;
+import pl.devcezz.animalshelter.commons.mail.EmailSender;
+import pl.devcezz.animalshelter.commons.mail.model.AdoptionMail;
+import pl.devcezz.animalshelter.read.AnimalProjection;
+import pl.devcezz.animalshelter.read.query.GetAnimalInfoQuery;
+import pl.devcezz.animalshelter.read.result.AnimalInfoDto;
 import pl.devcezz.cqrs.event.EventHandler;
 
 class HandleAdoptingAnimal implements EventHandler<AnimalAdoptionSucceeded> {
 
+    private final EmailSender emailSender;
+    private final AnimalProjection animalProjection;
+
+    HandleAdoptingAnimal(final EmailSender emailSender, final AnimalProjection animalProjection) {
+        this.emailSender = emailSender;
+        this.animalProjection = animalProjection;
+    }
+
     @Override
     public void handle(final AnimalAdoptionSucceeded event) {
-        System.out.println("Adopted: " + event.getAnimalId().value());
+        animalProjection.handle(new GetAnimalInfoQuery(event.getAnimalId().value()))
+                .peek(this::sendMail)
+                .getOrElseThrow(NotFoundAnimalInShelterException::new);
+    }
+
+    private void sendMail(final AnimalInfoDto animal) {
+        emailSender.sendEmail(new AdoptionMail(animal.getName()));
     }
 }
--- application/src/main/java/pl/devcezz/animalshelter/commons/mail/model/AdoptionMail.java
+++ application/src/main/java/pl/devcezz/animalshelter/commons/mail/model/AdoptionMail.java
@@ -3,18 +3,12 @@ package pl.devcezz.animalshelter.commons.mail.model;
 public class AdoptionMail {
 
     private final String name;
-    private final String species;
 
-    public AdoptionMail(final String name, final String species) {
+    public AdoptionMail(final String name) {
         this.name = name;
-        this.species = species;
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getSpecies() {
-        return species;
     }
 }

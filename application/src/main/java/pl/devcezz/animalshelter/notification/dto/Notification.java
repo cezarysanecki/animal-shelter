6c75a7ec206e8629fefcf7d4e9c4c248817fec9f
Cezary Sanecki <devcezz@gmail.com>
2021-11-10 10:51:17 +0100

    Added new type of notification for acceptance

--- application/src/main/java/pl/devcezz/animalshelter/notification/dto/Notification.java
+++ application/src/main/java/pl/devcezz/animalshelter/notification/dto/Notification.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 public interface Notification {
 
     enum NotificationType {
-        Adoption
+        Adoption, SuccessfulAcceptance, WarningAcceptance, FailureAcceptance
     }
 
     NotificationType type();
@@ -16,6 +16,32 @@ public interface Notification {
         @Override
         public NotificationType type() {
             return NotificationType.Adoption;
+        }
+    }
+
+    record SuccessfulAcceptanceNotification(UUID animalId, String animalName,
+                                            Integer animalAge, String animalSpecies) implements Notification {
+
+        @Override
+        public NotificationType type() {
+            return NotificationType.SuccessfulAcceptance;
+        }
+    }
+
+    record WarningAcceptanceNotification(UUID animalId, String animalName,
+                                         Integer animalAge, String animalSpecies) implements Notification {
+
+        @Override
+        public NotificationType type() {
+            return NotificationType.WarningAcceptance;
+        }
+    }
+
+    record FailureAcceptanceNotification(String reason) implements Notification {
+
+        @Override
+        public NotificationType type() {
+            return NotificationType.FailureAcceptance;
         }
     }
 }

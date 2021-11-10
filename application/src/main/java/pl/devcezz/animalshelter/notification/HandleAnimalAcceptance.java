f60bf63731f5c761ed62341cd227b969493085cd
Cezary Sanecki <devcezz@gmail.com>
2021-11-10 10:52:38 +0100

    Changed handlers for acceptance to use zookeeper repository

--- application/src/main/java/pl/devcezz/animalshelter/notification/HandleAnimalAcceptance.java
+++ application/src/main/java/pl/devcezz/animalshelter/notification/HandleAnimalAcceptance.java
@@ -1,30 +1,95 @@
 package pl.devcezz.animalshelter.notification;
 
-import pl.devcezz.cqrs.event.EventHandler;
+import io.vavr.collection.Set;
+import pl.devcezz.animalshelter.notification.dto.Notification.FailureAcceptanceNotification;
+import pl.devcezz.animalshelter.notification.dto.Notification.SuccessfulAcceptanceNotification;
+import pl.devcezz.animalshelter.notification.dto.Notification.WarningAcceptanceNotification;
 import pl.devcezz.animalshelter.shelter.event.AnimalEvent.AcceptingAnimalFailed;
-import pl.devcezz.animalshelter.shelter.event.AnimalEvent.AcceptingAnimalWarned;
 import pl.devcezz.animalshelter.shelter.event.AnimalEvent.AcceptingAnimalSucceeded;
+import pl.devcezz.animalshelter.shelter.event.AnimalEvent.AcceptingAnimalWarned;
+import pl.devcezz.cqrs.event.EventHandler;
 
-class HandleAcceptanceFailure implements EventHandler<AcceptingAnimalFailed> {
+class HandleSuccessfulAcceptance implements EventHandler<AcceptingAnimalSucceeded> {
+
+    private final ZookeeperContactRepository zookeeperContactRepository;
+    private final Set<Notifier> notifiers;
+
+    HandleSuccessfulAcceptance(final ZookeeperContactRepository zookeeperContactRepository, final Set<Notifier> notifiers) {
+        this.zookeeperContactRepository = zookeeperContactRepository;
+        this.notifiers = notifiers;
+    }
 
     @Override
-    public void handle(final AcceptingAnimalFailed event) {
-        System.out.println(event.reason());
+    public void handle(final AcceptingAnimalSucceeded event) {
+        Set<ZookeeperContact> contacts = zookeeperContactRepository.findAll();
+
+        notifiers.forEach(notifier -> notifier.notify(
+                contacts.map(ZookeeperContact::toZookeeperContactDetails),
+                createNotification(event))
+        );
+    }
+
+    private SuccessfulAcceptanceNotification createNotification(final AcceptingAnimalSucceeded event) {
+        return new SuccessfulAcceptanceNotification(
+                event.animalId(),
+                event.animalName(),
+                event.animalAge(),
+                event.animalSpecies()
+        );
     }
 }
 
 class HandleAcceptanceWarning implements EventHandler<AcceptingAnimalWarned> {
 
+    private final ZookeeperContactRepository zookeeperContactRepository;
+    private final Set<Notifier> notifiers;
+
+    HandleAcceptanceWarning(final ZookeeperContactRepository zookeeperContactRepository, final Set<Notifier> notifiers) {
+        this.zookeeperContactRepository = zookeeperContactRepository;
+        this.notifiers = notifiers;
+    }
+
     @Override
     public void handle(final AcceptingAnimalWarned event) {
-        System.out.println("Warned");
+        Set<ZookeeperContact> contacts = zookeeperContactRepository.findAll();
+
+        notifiers.forEach(notifier -> notifier.notify(
+                contacts.map(ZookeeperContact::toZookeeperContactDetails),
+                createNotification(event))
+        );
+    }
+
+    private WarningAcceptanceNotification createNotification(final AcceptingAnimalWarned event) {
+        return new WarningAcceptanceNotification(
+                event.animalId(),
+                event.animalName(),
+                event.animalAge(),
+                event.animalSpecies()
+        );
     }
 }
 
-class HandleAcceptanceSuccess implements EventHandler<AcceptingAnimalSucceeded> {
+class HandleAcceptanceFailure implements EventHandler<AcceptingAnimalFailed> {
+
+    private final ZookeeperContactRepository zookeeperContactRepository;
+    private final Set<Notifier> notifiers;
+
+    HandleAcceptanceFailure(final ZookeeperContactRepository zookeeperContactRepository, final Set<Notifier> notifiers) {
+        this.zookeeperContactRepository = zookeeperContactRepository;
+        this.notifiers = notifiers;
+    }
 
     @Override
-    public void handle(final AcceptingAnimalSucceeded event) {
-        System.out.println("Succeed");
+    public void handle(final AcceptingAnimalFailed event) {
+        Set<ZookeeperContact> contacts = zookeeperContactRepository.findAll();
+
+        notifiers.forEach(notifier -> notifier.notify(
+                contacts.map(ZookeeperContact::toZookeeperContactDetails),
+                createNotification(event))
+        );
+    }
+
+    private FailureAcceptanceNotification createNotification(final AcceptingAnimalFailed event) {
+        return new FailureAcceptanceNotification(event.reason());
     }
 }

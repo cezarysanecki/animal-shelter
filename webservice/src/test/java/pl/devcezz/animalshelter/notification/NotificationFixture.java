063bae4e51f1c0980df01f4b0bc3f1fe096018a7
Cezary Sanecki <devcezz@gmail.com>
2021-11-10 16:53:33 +0100

    Added IT test for acceptance notification

--- webservice/src/test/java/pl/devcezz/animalshelter/notification/NotificationFixture.java
+++ webservice/src/test/java/pl/devcezz/animalshelter/notification/NotificationFixture.java
@@ -2,6 +2,9 @@ package pl.devcezz.animalshelter.notification;
 
 import io.vavr.collection.HashSet;
 import io.vavr.collection.Set;
+import pl.devcezz.animalshelter.notification.dto.Notification.SuccessfulAcceptanceNotification;
+import pl.devcezz.animalshelter.notification.dto.Notification.WarnedAcceptanceNotification;
+import pl.devcezz.animalshelter.notification.dto.Notification.FailedAcceptanceNotification;
 import pl.devcezz.animalshelter.notification.dto.Notification.SuccessfulAdoptionNotification;
 import pl.devcezz.animalshelter.notification.dto.ZookeeperContactDetails;
 
@@ -15,5 +18,17 @@ class NotificationFixture {
 
     static SuccessfulAdoptionNotification successfulAdoptionNotification() {
         return new SuccessfulAdoptionNotification(UUID.randomUUID(), "Azor", 2, "Dog");
+    }
+
+    static SuccessfulAcceptanceNotification successfulAcceptanceNotification() {
+        return new SuccessfulAcceptanceNotification(UUID.randomUUID(), "Azor", 2, "Dog");
+    }
+
+    static WarnedAcceptanceNotification warnedAcceptanceNotification() {
+        return new WarnedAcceptanceNotification(UUID.randomUUID(), "Azor", 2, "Dog", "safe threshold is reached");
+    }
+
+    static FailedAcceptanceNotification failedAcceptanceNotification() {
+        return new FailedAcceptanceNotification("not enough space in shelter");
     }
 }

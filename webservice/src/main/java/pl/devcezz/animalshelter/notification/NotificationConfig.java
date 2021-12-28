53d32805587cbf3bc54d770ff26bc76789bd10f1
Cezary Sanecki <devcezz@gmail.com>
2021-12-28 16:49:46 +0100

    Added async invocation for email notifier

--- webservice/src/main/java/pl/devcezz/animalshelter/notification/NotificationConfig.java
+++ webservice/src/main/java/pl/devcezz/animalshelter/notification/NotificationConfig.java
@@ -2,9 +2,11 @@ package pl.devcezz.animalshelter.notification;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import pl.devcezz.animalshelter.notification.mail.EmailFacade;
 
 @Configuration
+@EnableAsync
 class NotificationConfig {
 
     @Bean

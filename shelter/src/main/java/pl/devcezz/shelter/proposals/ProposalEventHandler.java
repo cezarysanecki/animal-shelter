666604c2247ad693bc0132fc461b99ce45601ecd
Cezary Sanecki <devcezz@gmail.com>
2022-05-20 15:12:03 +0200

    Added missing transactional annotation

--- shelter/src/main/java/pl/devcezz/shelter/proposals/ProposalEventHandler.java
+++ shelter/src/main/java/pl/devcezz/shelter/proposals/ProposalEventHandler.java
@@ -5,12 +5,15 @@ import org.springframework.context.event.EventListener;
 import pl.devcezz.shelter.shared.event.AnimalCreatedEvent;
 import pl.devcezz.shelter.shared.event.AnimalDeletedEvent;
 
+import javax.transaction.Transactional;
+
 @RequiredArgsConstructor
 class ProposalEventHandler {
 
     private final AnimalProposalRepository animalProposalRepository;
 
     @EventListener
+    @Transactional
     public void handleCreatedAnimal(AnimalCreatedEvent event) {
         AnimalProposal animalProposal = AnimalProposal.newOne(
                 AnimalProposalId.of(event.getAnimalId()));
@@ -18,6 +21,7 @@ class ProposalEventHandler {
     }
 
     @EventListener
+    @Transactional
     public void handleDeletedAnimal(AnimalDeletedEvent event) {
         animalProposalRepository.deleteByAnimalProposalId(
                 AnimalProposalId.of(event.getAnimalId()));

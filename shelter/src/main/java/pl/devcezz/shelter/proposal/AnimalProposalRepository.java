944547985b38aef57632c8347e08d7748d00c3a9
Cezary Sanecki <devcezz@gmail.com>
2022-06-14 14:32:01 +0200

    Added method to find proposal by its id

--- shelter/src/main/java/pl/devcezz/shelter/proposal/AnimalProposalRepository.java
+++ shelter/src/main/java/pl/devcezz/shelter/proposal/AnimalProposalRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 interface AnimalProposalRepository extends JpaRepository<AnimalProposal, UUID> {
@@ -11,4 +12,6 @@ interface AnimalProposalRepository extends JpaRepository<AnimalProposal, UUID> {
     @Query("UPDATE AnimalProposal ap SET ap.status = 'DELETED'")
     @Modifying
     void deleteAnimalProposalByAnimalProposalId(AnimalProposalId animalProposalId);
+
+    Optional<AnimalProposal> findByAnimalProposalId(AnimalProposalId animalProposalId);
 }

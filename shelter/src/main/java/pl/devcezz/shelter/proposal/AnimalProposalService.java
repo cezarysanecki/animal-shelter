f3aa382ac7ba1673f9c0270d65c65304e7b886a6
Cezary Sanecki <devcezz@gmail.com>
2022-06-14 14:32:57 +0200

    Added service for handling proposals

---
+++ shelter/src/main/java/pl/devcezz/shelter/proposal/AnimalProposalService.java
@@ -0,0 +1,27 @@
+package pl.devcezz.shelter.proposal;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import pl.devcezz.shelter.proposal.exception.AnimalProposalNotFound;
+
+@Transactional("proposalTransactionManager")
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+class AnimalProposalService {
+
+    private final AnimalProposalRepository animalProposalRepository;
+
+    void acceptProposal(AnimalProposalId animalProposalId) {
+        AnimalProposal animalProposal = animalProposalRepository.findByAnimalProposalId(animalProposalId)
+                .orElseThrow(() -> new AnimalProposalNotFound(animalProposalId.getValue()));
+
+        animalProposal.accept();
+    }
+
+    void declineProposal(AnimalProposalId animalProposalId) {
+        AnimalProposal animalProposal = animalProposalRepository.findByAnimalProposalId(animalProposalId)
+                .orElseThrow(() -> new AnimalProposalNotFound(animalProposalId.getValue()));
+
+        animalProposal.decline();
+    }
+}

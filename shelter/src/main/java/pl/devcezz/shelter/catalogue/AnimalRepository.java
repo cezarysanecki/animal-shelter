136dbb18dd99b7566b676958d748253cb6bc961d
Cezary Sanecki <devcezz@gmail.com>
2022-06-18 10:31:23 +0200

    Added query to set animal data as deleted

--- shelter/src/main/java/pl/devcezz/shelter/catalogue/AnimalRepository.java
+++ shelter/src/main/java/pl/devcezz/shelter/catalogue/AnimalRepository.java
@@ -1,6 +1,9 @@
 package pl.devcezz.shelter.catalogue;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import pl.devcezz.shelter.proposal.AnimalProposalId;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -8,4 +11,8 @@ import java.util.UUID;
 interface AnimalRepository extends CrudRepository<Animal, UUID> {
 
     Optional<Animal> findByAnimalId(AnimalId animalId);
+
+    @Query("UPDATE Animal a SET a.status = 'DELETED' WHERE a.animalId = ?1")
+    @Modifying
+    void deleteAnimalByAnimalId(AnimalId animalId);
 }

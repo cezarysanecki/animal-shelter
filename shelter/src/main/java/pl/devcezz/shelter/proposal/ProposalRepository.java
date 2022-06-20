c9369c492839c8d8c1c378dcd20678a836e2f372
Cezary Sanecki <devcezz@gmail.com>
2022-06-20 16:27:10 +0200

    Changed to crud repo

--- shelter/src/main/java/pl/devcezz/shelter/proposal/ProposalRepository.java
+++ shelter/src/main/java/pl/devcezz/shelter/proposal/ProposalRepository.java
@@ -1,12 +1,12 @@
 package pl.devcezz.shelter.proposal;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import pl.devcezz.shelter.proposal.exception.ProposalNotFoundException;
 
 import java.util.Optional;
 import java.util.UUID;
 
-interface ProposalRepository extends JpaRepository<Proposal, UUID> {
+interface ProposalRepository extends CrudRepository<Proposal, UUID> {
 
     Optional<Proposal> findFirstBySubjectIdOrderByCreationTimestampDesc(SubjectId subjectId);
 

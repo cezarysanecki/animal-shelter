ea19ac417af3279db5c49137b69bdde149e8f485
Cezary Sanecki <devcezz@gmail.com>
2022-06-20 22:24:27 +0200

    Added equals and hashCode for AnimalId

--- shelter/src/main/java/pl/devcezz/shelter/catalogue/AnimalId.java
+++ shelter/src/main/java/pl/devcezz/shelter/catalogue/AnimalId.java
@@ -1,6 +1,7 @@
 package pl.devcezz.shelter.catalogue;
 
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Access;
@@ -11,6 +12,7 @@ import java.util.UUID;
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Access(AccessType.FIELD)
+@EqualsAndHashCode
 class AnimalId {
 
     private String value;

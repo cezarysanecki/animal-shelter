1c5a4770c38787d88e1c67e9b48690e8eeedc807
Cezary Sanecki <devcezz@gmail.com>
2022-06-20 23:33:17 +0200

    Added another part of unit tests for proposal

--- shelter/src/test/groovy/pl/devcezz/shelter/proposal/ProposalTest.groovy
+++ shelter/src/test/groovy/pl/devcezz/shelter/proposal/ProposalTest.groovy
@@ -1,7 +1,9 @@
 package pl.devcezz.shelter.proposal
 
 import org.springframework.context.ApplicationEventPublisher
+import pl.devcezz.shelter.proposal.exception.ProposalIllegalStateException
 import pl.devcezz.shelter.shared.event.AnimalCreatedEvent
+import pl.devcezz.shelter.shared.event.AnimalDeletedEvent
 import spock.lang.Specification
 
 class ProposalTest extends Specification {
@@ -35,8 +37,78 @@ class ProposalTest extends Specification {
             proposal.getStatus().name() == "ACCEPTED"
     }
 
+    def 'should decline proposal'() {
+        given:
+            def subjectUuid = anySubjectUuid()
+
+        when:
+            handleCreationOfProposalFor(subjectUuid)
+        and:
+            def proposal = getProposal(subjectUuid)
+
+        then:
+            proposal.getStatus().name() == "PENDING"
+
+        when:
+            facade.declineProposal(subjectUuid)
+        and:
+            proposal = getProposal(subjectUuid)
+
+        then:
+            proposal.getStatus().name() == "DECLINED"
+    }
+
+    def 'should fail when accept or decline proposal which is deleted'() {
+        given:
+            def subjectUuid = anySubjectUuid()
+
+        when:
+            handleCreationOfProposalFor(subjectUuid)
+        and:
+            handleDeletionOfProposalFor(subjectUuid)
+        and:
+            def proposal = getProposal(subjectUuid)
+
+        then:
+            proposal.getStatus().name() == "DELETED"
+
+        when:
+            facade.acceptProposal(subjectUuid)
+
+        then:
+            def error = thrown(ProposalIllegalStateException.class)
+            error.getMessage().contains("accept")
+
+        when:
+            facade.declineProposal(subjectUuid)
+
+        then:
+            error = thrown(ProposalIllegalStateException.class)
+            error.getMessage().contains("decline")
+    }
+
+    def 'should fail when delete proposal which is already deleted'() {
+        given:
+            def subjectUuid = anySubjectUuid()
+
+        when:
+            handleCreationOfProposalFor(subjectUuid)
+        and:
+            handleDeletionOfProposalFor(subjectUuid)
+        and:
+            handleDeletionOfProposalFor(subjectUuid)
+
+        then:
+            def error = thrown(ProposalIllegalStateException.class)
+            error.getMessage().contains("delete")
+    }
+
     private handleCreationOfProposalFor(UUID subjectUuid) {
         handler.handleCreatedAnimal(new AnimalCreatedEvent(subjectUuid))
+    }
+
+    private handleDeletionOfProposalFor(UUID subjectUuid) {
+        handler.handleDeletedAnimal(new AnimalDeletedEvent(subjectUuid))
     }
 
     private Proposal getProposal(UUID subjectUuid) {

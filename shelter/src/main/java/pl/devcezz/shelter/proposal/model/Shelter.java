a579c64186d27ce59d669646376429b2334c003d
Cezary Sanecki <devcezz@gmail.com>
2022-06-21 23:38:02 +0200

    Added concept of shelter with events

---
+++ shelter/src/main/java/pl/devcezz/shelter/proposal/model/Shelter.java
@@ -0,0 +1,49 @@
+package pl.devcezz.shelter.proposal.model;
+
+import io.vavr.control.Either;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import pl.devcezz.shelter.proposal.model.ShelterEvent.ProposalAcceptedEvents;
+import pl.devcezz.shelter.proposal.model.ShelterEvent.ProposalAcceptedFailed;
+import pl.devcezz.shelter.proposal.model.ShelterEvent.SafeThresholdExceeded;
+
+import static pl.devcezz.shelter.proposal.model.ShelterEvent.ProposalAccepted;
+import static pl.devcezz.shelter.proposal.model.ShelterEvent.ProposalAccepted.*;
+import static pl.devcezz.shelter.proposal.model.ShelterEvent.ProposalAcceptedEvents.events;
+import static pl.devcezz.shelter.proposal.model.ShelterEvent.ProposalAcceptedFailed.proposalAcceptedFailedNow;
+import static pl.devcezz.shelter.shared.event.EitherResult.announceFailure;
+import static pl.devcezz.shelter.shared.event.EitherResult.announceSuccess;
+
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+public class Shelter {
+
+    private static final int CAPACITY = 10;
+    private static final int SAFE_THRESHOLD = 7;
+
+    @NonNull
+    private final int acceptedProposalsCount;
+
+    Either<ProposalAcceptedFailed, ProposalAcceptedEvents> accept(PendingProposal pendingProposal) {
+        if (enoughSpaceInShelterAfterAccepting()) {
+            ProposalAccepted proposalAccepted = proposalAcceptedNow(pendingProposal.getProposalId());
+            if (safeThresholdExceededAfterAccepting()) {
+                return announceSuccess(events(proposalAccepted, SafeThresholdExceeded.now(pendingProposal.getProposalId(), countSpaceLeft())));
+            }
+            return announceSuccess(events(proposalAccepted));
+        }
+        return announceFailure(proposalAcceptedFailedNow("no space left for animals in shelter", pendingProposal.getProposalId()));
+    }
+
+    private int countSpaceLeft() {
+        return CAPACITY - (acceptedProposalsCount + 1);
+    }
+
+    private boolean enoughSpaceInShelterAfterAccepting() {
+        return CAPACITY >= acceptedProposalsCount + 1;
+    }
+
+    private boolean safeThresholdExceededAfterAccepting() {
+        return SAFE_THRESHOLD <= acceptedProposalsCount + 1;
+    }
+}
---
+++ shelter/src/main/java/pl/devcezz/shelter/proposal/model/ShelterEvent.java
@@ -0,0 +1,66 @@
+package pl.devcezz.shelter.proposal.model;
+
+import io.vavr.control.Option;
+import lombok.NonNull;
+import lombok.Value;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public interface ShelterEvent {
+
+    @Value
+    class ProposalAccepted implements ShelterEvent {
+        @NonNull Instant when;
+        @NonNull UUID proposalId;
+
+        public static ProposalAccepted proposalAcceptedNow(ProposalId proposalId) {
+            return new ProposalAccepted(
+                    Instant.now(),
+                    proposalId.getValue());
+        }
+    }
+
+    @Value
+    class SafeThresholdExceeded implements ShelterEvent {
+        @NonNull Instant when;
+        @NonNull UUID proposalId;
+        int spaceLeft;
+
+        public static SafeThresholdExceeded now(ProposalId proposalId, int spaceLeft) {
+            return new SafeThresholdExceeded(
+                    Instant.now(),
+                    proposalId.getValue(),
+                    spaceLeft);
+        }
+    }
+
+    @Value
+    class ProposalAcceptedEvents implements ShelterEvent {
+        @NonNull UUID proposalId;
+        @NonNull ProposalAccepted proposalAccepted;
+        @NonNull Option<SafeThresholdExceeded> safeThresholdExceeded;
+
+        public static ProposalAcceptedEvents events(ProposalAccepted proposalAccepted) {
+            return new ProposalAcceptedEvents(proposalAccepted.getProposalId(), proposalAccepted, Option.none());
+        }
+
+        public static ProposalAcceptedEvents events(ProposalAccepted proposalAccepted, SafeThresholdExceeded safeThresholdExceeded) {
+            return new ProposalAcceptedEvents(proposalAccepted.getProposalId(), proposalAccepted, Option.of(safeThresholdExceeded));
+        }
+    }
+
+    @Value
+    class ProposalAcceptedFailed implements ShelterEvent {
+        @NonNull String reason;
+        @NonNull Instant when;
+        @NonNull UUID proposalId;
+
+        static ProposalAcceptedFailed proposalAcceptedFailedNow(String reason, ProposalId proposalId) {
+            return new ProposalAcceptedFailed(
+                    reason,
+                    Instant.now(),
+                    proposalId.getValue());
+        }
+    }
+}

690a4c0a254859f5b108c6ed17ad14aa913e1acc
Cezary Sanecki <devcezz@gmail.com>
2022-07-08 23:59:56 +0200

    Extracted web context for controller tests in adoption module

--- shelter/src/integration-test/groovy/pl/devcezz/shelter/adoption/shelter/web/ShelterControllerIT.groovy
+++ shelter/src/integration-test/groovy/pl/devcezz/shelter/adoption/shelter/web/ShelterControllerIT.groovy
@@ -1,38 +1,18 @@
 package pl.devcezz.shelter.adoption.shelter.web
 
 import io.vavr.control.Try
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.test.web.servlet.MockMvc
-import pl.devcezz.shelter.adoption.AdoptionTestContext
-import pl.devcezz.shelter.adoption.shelter.application.AcceptingProposal
-import pl.devcezz.shelter.adoption.shelter.application.CancelingProposal
+import pl.devcezz.shelter.adoption.AdoptionTestWebContext
 import pl.devcezz.shelter.commons.commands.Result
-import spock.lang.Specification
 
 import static org.mockito.ArgumentMatchers.any
 import static org.mockito.BDDMockito.given
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest(ShelterController.class)
-@ContextConfiguration(classes = [AdoptionTestContext.class])
-class ShelterControllerIT extends Specification {
-
-    @Autowired
-    private MockMvc mvc
-
-    @MockBean
-    private AcceptingProposal acceptingProposal
-
-    @MockBean
-    private CancelingProposal cancelingProposal
+class ShelterControllerIT extends AdoptionTestWebContext {
 
     def "should accept proposal"() {
         given: "Prepare proposal to accept."

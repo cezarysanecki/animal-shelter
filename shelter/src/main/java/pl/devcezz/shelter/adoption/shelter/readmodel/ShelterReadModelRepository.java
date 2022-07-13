3d538ecb1bc5632ff8e623a6c32f14cc43bb7be1
Cezary Sanecki <devcezz@gmail.com>
2022-07-13 22:36:36 +0200

    Fixed jdbc queries

--- shelter/src/main/java/pl/devcezz/shelter/adoption/shelter/readmodel/ShelterReadModelRepository.java
+++ shelter/src/main/java/pl/devcezz/shelter/adoption/shelter/readmodel/ShelterReadModelRepository.java
@@ -3,9 +3,13 @@ package pl.devcezz.shelter.adoption.shelter.readmodel;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import pl.devcezz.shelter.adoption.shelter.readmodel.dto.ShelterProposalDto;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class ShelterReadModelRepository {
@@ -13,9 +17,20 @@ class ShelterReadModelRepository {
     private final JdbcTemplate jdbcTemplate;
 
     List<ShelterProposalDto> findAcceptedProposals() {
-        return jdbcTemplate.queryForList("" +
+        return jdbcTemplate.query("" +
                         "SELECT a.proposal_id, a.status " +
                         "FROM accepted_proposal_database_entity a",
-                ShelterProposalDto.class);
+                new ShelterProposalRowMapper());
+    }
+}
+
+class ShelterProposalRowMapper implements RowMapper<ShelterProposalDto> {
+
+    @Override
+    public ShelterProposalDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return ShelterProposalDto.builder()
+                .proposalId(UUID.fromString(rs.getString("proposal_id")))
+                .status(rs.getString("status"))
+                .build();
     }
 }
--- shelter/src/main/java/pl/devcezz/shelter/catalogue/CatalogueReadModelRepository.java
+++ shelter/src/main/java/pl/devcezz/shelter/catalogue/CatalogueReadModelRepository.java
@@ -2,23 +2,42 @@ package pl.devcezz.shelter.catalogue;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public class CatalogueReadModelRepository {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcOperations jdbcTemplate;
 
     public List<AnimalDto> findAllFor(List<UUID> animalIds) {
-        return jdbcTemplate.queryForList("" +
+        return jdbcTemplate.query("" +
                         "SELECT a.animal_id, a.name, a.age, a.species, a.gender " +
-                        "FROM animal a" +
-                        "WHERE a.animal_id IN ?",
-                AnimalDto.class,
-                animalIds.stream().map(UUID::toString).toList());
+                        "FROM animal a " +
+                        "WHERE a.animal_id IN (:animalIds)",
+                Map.of("animalIds", animalIds.stream().map(Object::toString).toList()),
+                new AnimalRowMapper());
+    }
+}
+
+class AnimalRowMapper implements RowMapper<AnimalDto> {
+
+    @Override
+    public AnimalDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return AnimalDto.builder()
+                .animalId(UUID.fromString(rs.getString("animal_id")))
+                .name(rs.getString("name"))
+                .age(rs.getInt("age"))
+                .species(rs.getString("species"))
+                .gender(rs.getString("gender"))
+                .build();
     }
 }
 

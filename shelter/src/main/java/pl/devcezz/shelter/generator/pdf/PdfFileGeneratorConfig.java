0f8efbcc87d98e482855057a561a08e05e46c5ea
Cezary Sanecki <devcezz@gmail.com>
2022-07-14 14:42:41 +0200

    Used generic type for file generator bean

--- shelter/src/main/java/pl/devcezz/shelter/generator/pdf/PdfFileGeneratorConfig.java
+++ shelter/src/main/java/pl/devcezz/shelter/generator/pdf/PdfFileGeneratorConfig.java
@@ -3,12 +3,13 @@ package pl.devcezz.shelter.generator.pdf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.TemplateEngine;
+import pl.devcezz.shelter.generator.FileGenerator;
 
 @Configuration
 public class PdfFileGeneratorConfig {
 
     @Bean
-    PdfGenerator pdfGenerator(TemplateEngine templateEngine) {
+    FileGenerator pdfGenerator(TemplateEngine templateEngine) {
         HtmlContentGenerator htmlContentGenerator = new HtmlContentGenerator(templateEngine);
         PdfCreator pdfCreator = new PdfCreator();
         return new PdfGenerator(htmlContentGenerator, pdfCreator);

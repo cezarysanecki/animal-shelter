679cc4d817927e9b56597ae266eb3ccc124728f5
Cezary Sanecki <devcezz@gmail.com>
2022-08-22 19:00:05 +0200

    Added view handler in proposal

--- shelter/src/main/java/pl/devcezz/shelter/adoption/proposal/application/CatalogueOperationsEventsHandler.java
+++ shelter/src/main/java/pl/devcezz/shelter/adoption/proposal/application/CatalogueOperationsEventsHandler.java
@@ -1,10 +1,9 @@
 package pl.devcezz.shelter.adoption.proposal.application;
 
 import lombok.RequiredArgsConstructor;
+import pl.devcezz.shelter.adoption.proposal.model.Proposal;
 import pl.devcezz.shelter.adoption.proposal.model.ProposalId;
 import pl.devcezz.shelter.adoption.proposal.model.Proposals;
-import pl.devcezz.shelter.adoption.proposal.view.ProposalAnimalData;
-import pl.devcezz.shelter.adoption.proposal.view.ProposalAnimalDatabase;
 import pl.devcezz.shelter.commons.events.handler.EventsListener;
 import pl.devcezz.shelter.commons.infrastructure.AdoptionTransactional;
 
@@ -15,16 +14,16 @@ import static pl.devcezz.shelter.catalogue.CatalogueEvent.AnimalConfirmedEvent;
 public class CatalogueOperationsEventsHandler {
 
     private final Proposals proposalRepository;
-    private final ProposalAnimalDatabase animalDatabase;
 
     @EventsListener
     @AdoptionTransactional
     public void handle(AnimalConfirmedEvent event) {
-        ProposalId proposalId = ProposalId.of(event.getAnimalId());
+        saveProposal(
+                createNewPendingProposal(ProposalId.of(event.getAnimalId())));
+    }
 
-        proposalRepository.save(
-                createNewPendingProposal(proposalId));
-        animalDatabase.save(
-                ProposalAnimalData.of(proposalId, event.getName(), event.getAge(), event.getSpecies(), event.getGender()));
+    private Proposal saveProposal(Proposal proposal) {
+        proposalRepository.save(proposal);
+        return proposal;
     }
 }
--- shelter/src/main/java/pl/devcezz/shelter/adoption/proposal/view/ProposalAnimalDatabase.java
+++ shelter/src/main/java/pl/devcezz/shelter/adoption/proposal/view/ProposalAnimalDatabase.java
@@ -1,27 +1,7 @@
 package pl.devcezz.shelter.adoption.proposal.view;
 
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.repository.CrudRepository;
 
-@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
-public class ProposalAnimalDatabase {
+interface ProposalAnimalDatabase extends CrudRepository<ProposalAnimalData, Long> {
 
-    private final JdbcTemplate jdbcTemplate;
-
-    public void save(ProposalAnimalData animalData) {
-        insert(animalData);
-    }
-
-    private int insert(ProposalAnimalData animalData) {
-        return jdbcTemplate.update("" +
-                        "INSERT INTO proposal_animal " +
-                        "(proposal_id, name, age, species, gender) VALUES " +
-                        "(?, ?, ?, ?, ?)",
-                animalData.getProposalId().getValue(),
-                animalData.getName().getValue(),
-                animalData.getAge().getValue(),
-                animalData.getSpecies().getValue(),
-                animalData.getGender().toString());
-    }
 }
